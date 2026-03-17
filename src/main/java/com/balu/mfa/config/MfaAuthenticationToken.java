bd2f12de7ed672a11fe22a37cc33986285e7b9d2
BALASUBRAMANYA <N.balasubramanya@esyasoft.com>
2026-03-17 15:11:36 +0530

    Clean up

--- src/main/java/com/balu/mfa/config/MfaAuthenticationToken.java
+++ src/main/java/com/balu/mfa/config/MfaAuthenticationToken.java
@@ -2,9 +2,9 @@ package com.balu.mfa.config;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-public class MfaAuthenticationToken extends UsernamePasswordAuthenticationToken {
-
-    public MfaAuthenticationToken(Object principal) {
-        super(principal, null);
-    }
-}
+//public class MfaAuthenticationToken extends UsernamePasswordAuthenticationToken {
+//
+//    public MfaAuthenticationToken(Object principal) {
+//        super(principal, null);
+//    }
+//}
